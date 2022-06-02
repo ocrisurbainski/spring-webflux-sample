b85166df24822618b83c994ee33a2827984fba3c
cristian.urbainski <cristianurbainskips@gmail.com>
2022-06-02 16:09:05 -0300

    Adicionado o swagger no projeto. Adicionado o contextPath de /api na aplicação. Atualizado collections do postman.

--- src/main/java/br/com/urbainski/springwebfluxsample/people/infra/controller/PeopleController.java
+++ src/main/java/br/com/urbainski/springwebfluxsample/people/infra/controller/PeopleController.java
@@ -1,6 +1,8 @@
 package br.com.urbainski.springwebfluxsample.people.infra.controller;
 
 import br.com.urbainski.springwebfluxsample.people.PeopleOperations;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -9,8 +11,8 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
-@RequestMapping("/people")
 @AllArgsConstructor
+@RequestMapping("/people")
 public class PeopleController {
 
     private final PeopleOperations operations;
@@ -18,6 +20,10 @@ public class PeopleController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @Operation(operationId = "insert", description = "Método para salvar uma nova pessoa", responses = {
+            @ApiResponse(responseCode = "201", description = "Pessoa salva com sucesso"),
+            @ApiResponse(responseCode = "500", description = "Caso algum erro aconteca")
+    })
     public Mono<PeopleDTO> insert(@RequestBody PeopleDTO dto) {
         var people = mapper.toPeople(dto);
         return operations.insert(people)
@@ -27,6 +33,10 @@ public class PeopleController {
 
     @PutMapping
     @ResponseStatus(HttpStatus.OK)
+    @Operation(operationId = "update", description = "Método para atualizar uma pessoa", responses = {
+            @ApiResponse(responseCode = "200", description = "Pessoa atualizada com sucesso"),
+            @ApiResponse(responseCode = "500", description = "Caso algum erro aconteca")
+    })
     public Mono<PeopleDTO> update(@RequestBody PeopleDTO dto) {
         var people = mapper.toPeople(dto);
         return operations.update(people)
@@ -34,15 +44,23 @@ public class PeopleController {
                 .log();
     }
 
-    @RequestMapping("/{id}")
+    @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
+    @Operation(description = "Método para pesquisar uma pessoa pelo sue identificador", responses = {
+            @ApiResponse(responseCode = "200", description = "Pessoa encontrado com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Caso a pessoa não seja encontrada pelo seu identificador"),
+            @ApiResponse(responseCode = "500", description = "Caso algum outro erro aconteca")
+    })
     public Mono<PeopleDTO> findById(@PathVariable("id") String id) {
         return operations.findById(id)
                 .map(mapper::toPeopleDTO)
                 .log();
     }
 
-    @RequestMapping(produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    @GetMapping(value = "/all", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    @Operation(operationId = "findAll", description = "Método para listar todas as pessoas", responses = {
+            @ApiResponse(responseCode = "200", description = "Pessoas listadas com sucesso")
+    })
     public Flux<PeopleDTO> findAll() {
         return operations.findAll()
                 .map(mapper::toPeopleDTO)
@@ -51,6 +69,11 @@ public class PeopleController {
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
+    @Operation(operationId = "deleteById", description = "Método para deletar uma pessoa", responses = {
+            @ApiResponse(responseCode = "200", description = "Pessoa excluída com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Caso a pessoa não seja encontrada pelo seu identificador"),
+            @ApiResponse(responseCode = "500", description = "Caso algum outro erro aconteca")
+    })
     public Mono<Void> deleteById(@PathVariable("id") String id) {
         return operations.deleteById(id).log();
     }
